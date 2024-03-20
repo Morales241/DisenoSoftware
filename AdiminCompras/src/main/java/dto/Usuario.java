849048eb46c7913fb5b9b02b96fdce2b5db393b7
Macs233219 <144395765+Macs233219@users.noreply.github.com>
2024-03-19 23:58:09 -0700

    DTOs
    Creación de DTOs

---
+++ AdiminCompras/src/main/java/dto/Usuario.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+
+/**
+ *
+ * @author marlon
+ */
+public class Usuario {
+    
+    private Long id;
+    private String nombre;
+    private String correoElectronico;
+    private String contrasena;
+
+    public Usuario() {
+    }
+    
+    public Usuario(Long id, String nombre, String correoElectronico, String contrasena) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correoElectronico = correoElectronico;
+        this.contrasena = contrasena;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    
+    
+}
