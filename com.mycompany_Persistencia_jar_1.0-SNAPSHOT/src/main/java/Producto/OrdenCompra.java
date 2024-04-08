fe06e5693e7c0ab928192f42c59da3c760c9c088
Macs233219 <144395765+Macs233219@users.noreply.github.com>
2024-04-07 17:35:15 -0700

    Paquete de Entidades
    Modificación de entidad OrdenCompra y nueva entidad Finanzas

--- com.mycompany_Persistencia_jar_1.0-SNAPSHOT/src/main/java/Producto/OrdenCompra.java
+++ com.mycompany_Persistencia_jar_1.0-SNAPSHOT/src/main/java/Producto/OrdenCompra.java
@@ -7,6 +7,7 @@ package Producto;
 import Producto.ProComprado;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -15,6 +16,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
@@ -33,16 +36,28 @@ public class OrdenCompra implements Serializable {
     
     @OneToMany(mappedBy = "orden", cascade= {CascadeType.PERSIST,CascadeType.MERGE, CascadeType.REMOVE})
     private List<ProComprado> productos;
+    
+    @Temporal(TemporalType.DATE)
+    private Date fechaExpedicion;
 
     public OrdenCompra() {
-        this.productos = new ArrayList<>();
     }
 
-    public OrdenCompra(Double total, List<ProComprado> productos) {
+    public OrdenCompra(Long id, Double total, List<ProComprado> productos, Date fechaExpedicion) {
+        this.id = id;
         this.total = total;
         this.productos = productos;
+        this.fechaExpedicion = fechaExpedicion;
     }
-    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Double getTotal() {
         return total;
     }
@@ -58,14 +73,13 @@ public class OrdenCompra implements Serializable {
     public void setProductos(List<ProComprado> productos) {
         this.productos = productos;
     }
-    
-    public Long getId() {
-        return id;
+
+    public Date getFechaExpedicion() {
+        return fechaExpedicion;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setFechaExpedicion(Date fechaExpedicion) {
+        this.fechaExpedicion = fechaExpedicion;
     }
-
     
 }
