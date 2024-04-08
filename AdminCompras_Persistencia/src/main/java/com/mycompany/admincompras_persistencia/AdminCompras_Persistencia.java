df647d0db0438ad1951536d7f5e28056b2cae8d2
SUSMOR <163379761+Morales241@users.noreply.github.com>
2024-04-07 19:03:33 -0700

    lo

---
+++ AdminCompras_Persistencia/src/main/java/com/mycompany/admincompras_persistencia/AdminCompras_Persistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.admincompras_persistencia;
+
+/**
+ *
+ * @author tacot
+ */
+public class AdminCompras_Persistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
