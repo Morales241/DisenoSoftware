f92603bf4e78a8cfd724e8ca423ca4f50c926a55
stae01 <166432863+stae01@users.noreply.github.com>
2024-04-10 01:29:37 -0700

    Subsistema inicio sesión

---
+++ AdminCompras_Negocio/src/main/java/subSistemaInicioSesion/inicioSesion.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package subSistemaInicioSesion;
+
+
+/**
+ *
+ * @author 
+ */
+public class inicioSesion {
+    
+    
+    public boolean inicioSesion(String usuario, String contraseña){
+        
+        if (usuario.equals(usuario) && contraseña.equals(contraseña)) {
+            return true;
+        }
+        return false;
+    }
+    
+}
